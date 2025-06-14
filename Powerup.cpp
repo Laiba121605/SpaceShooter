feb600a1db45b9a9af970194323a16a124c549b6
bscs24128 <bscs24128@itu.edu.pk>
2025-06-14 06:00:09 +0500

    Class PowerUp

---
+++ Powerup.cpp
@@ -0,0 +1,81 @@
+#include"Powerup.h"
+
+PowerUp::PowerUp(int x, int y, Color c, int dur)
+	: positionx(x), positiony(y), radius(20),
+	color(c), active(true), duration(dur)
+{}
+
+void PowerUp::draw() const
+{
+	if (active)
+	{
+		DrawCircle(positionx, positiony, (float)radius, color);
+	}
+}
+
+int PowerUp::getx() const
+{
+	return positionx;
+}
+
+int PowerUp::gety() const
+{
+	return positiony;
+}
+
+int PowerUp::getradius() const
+{
+	return radius;
+}
+
+bool PowerUp::isactive() const
+{
+	return active;
+}
+
+void PowerUp::deactivate()
+{
+	active = false;
+}
+
+int PowerUp::getduration() const
+{
+	return duration;
+}
+
+MaxHealthPowerUp::MaxHealthPowerUp(int x, int y) : PowerUp(x, y, PINK, 1) 
+{}
+
+void MaxHealthPowerUp::apply(PlayerShip& player)
+{
+	player.heal(player.getmaxhealth());
+	deactivate();
+}
+
+TwiceSpeedPowerUp::TwiceSpeedPowerUp(int x, int y) : PowerUp(x, y, PURPLE, 420)
+{}
+
+void TwiceSpeedPowerUp::apply(PlayerShip& player)
+{
+	player.setspeed(player.getspeed() * 2.0f);
+	deactivate();
+}
+
+void TwiceSpeedPowerUp::remove(PlayerShip& player)
+{
+	player.setspeed(player.getspeed() / 2.0f);
+}
+
+TwiceDamagePowerUp::TwiceDamagePowerUp(int x, int y) : PowerUp(x, y, ORANGE, 420)
+{}
+
+void TwiceDamagePowerUp::apply(PlayerShip& player)
+{
+	player.setdamage(player.getdamage() * 2.0f);
+	deactivate();
+}
+
+void TwiceDamagePowerUp::remove(PlayerShip& player)
+{
+	player.setdamage(player.getdamage() / 2.0f);
+}
---
+++ Powerup.h
@@ -0,0 +1,55 @@
+#ifndef POWERUP_H
+#define POWERUP_H
+
+#include "raylib.h"
+#include "PlayerShip.h"
+#include <iostream>
+using namespace std;
+
+class PowerUp 
+{
+protected:
+    int positionx;
+    int positiony;
+    int radius;
+    Color color;
+    bool active;
+    int duration;  // baiscally the number of frames the effect lasts
+public:
+    PowerUp(int x, int y, Color c, int dur = 300);
+    virtual ~PowerUp() {}
+    virtual void apply(PlayerShip& player) = 0;
+    virtual void remove(PlayerShip& player) = 0;
+    void draw() const;
+    int getx() const;
+    int gety() const;
+    int getradius() const;
+    bool isactive() const;
+    void deactivate();
+    int getduration() const;
+};
+
+class MaxHealthPowerUp : public PowerUp
+{
+public:
+    MaxHealthPowerUp(int x, int y);
+    void apply(PlayerShip& player) override;
+    void remove(PlayerShip& player) override {}
+};
+
+class TwiceSpeedPowerUp : public PowerUp 
+{
+public:
+    TwiceSpeedPowerUp(int x, int y);
+    void apply(PlayerShip& player) override;
+    void remove(PlayerShip& player) override;
+};
+
+class TwiceDamagePowerUp : public PowerUp
+{
+public:
+    TwiceDamagePowerUp(int x, int y);
+    void apply(PlayerShip& player) override;
+    void remove(PlayerShip& player) override;
+};
+#endif
